808deb764bb8210962560453c65875a6ab1e5c49
Veronica Miller <vmcmiller@gmail.com>
2019-02-25 03:33:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  };
 
+  yearsExperienceFromBeginningOf(year) {
+    return new Date("Jan 01, " + year).getFullYear() - this.startDate.getFullYear();
+  };
+};
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  };
+
+  blocksTravelled() {
+    let eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
+    function horizontalNumber(avenue) {
+
+    };
+    //const horizontalDistance =
+    //eastWest.findIndex(function(avenue) {return avenue === this.endingLocation.horizontal});
+    //}) - eastWest.findIndex(function(avenue) {return avenue === this.beginningLocation.horizontal});
+
+
+    const verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical;
+
+    return verticalDistance;
+  };
+};

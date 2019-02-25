c762aaf032184f93e5265a2f1d6b6a38c769afa3
Veronica Miller <vmcmiller@gmail.com>
2019-02-25 05:07:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,10 +38,10 @@ class Route {
 
   estimatedTime(peak) {
     if(peak) {
-      return blocksTravelled()/2;
+      return this.blocksTravelled()/2;
     }
     else {
-      return blocksTravelled()/3;
+      return this.blocksTravelled()/3;
     }
   };
 };
